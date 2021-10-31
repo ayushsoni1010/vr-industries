1c4e8e4bd79735f5a4779df089c5f32b5cd0dd73
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-31 19:57:12 +0530

    🚀update: product card

--- src/components/widgets/Cards/ProductCard/index.js
+++ src/components/widgets/Cards/ProductCard/index.js
@@ -18,12 +18,29 @@ export default function Cards(__cardProperties) {
     );
   } else {
     return (
-      <div className="produc-card-wrapper">
-        <img src={__cardProperties.productImage} alt="ProductImage" />
-        <p className="product-card-title">{__cardProperties.title}</p>
-        <p className="product-card-content-description">
-          {__cardProperties.description}
-        </p>
+      <div className="product-card-specifications-wrapper">
+        <div className="product-images-wrapper">
+          <img src={__cardProperties.productImage} alt="ProductImage" />
+        </div>
+        <div className="product-card-content-wrapper">
+          <div className="product-description-wrapper">
+            <h1>PRODUCT DESCRIPTION :</h1>
+            <p className="product-card-title">{__cardProperties.title}</p>
+            <p className="product-card-content-description">
+              {__cardProperties.description}
+            </p>
+          </div>
+          <div className="product-description-wrapper">
+            <h1>PRODUCT SPECIFICATIONS :</h1>
+            <ul>
+              <li><span>{__cardProperties.key1}</span>  {__cardProperties.point1}</li>
+              <li><span>{__cardProperties.key2}</span>{__cardProperties.point2}</li>
+              <li><span>{__cardProperties.key3}</span>{__cardProperties.point3}</li>
+              <li><span>{__cardProperties.key4}</span>{__cardProperties.point4}</li>
+              <li><span>{__cardProperties.key5}</span>{__cardProperties.point5}</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
